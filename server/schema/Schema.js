ff4d57f1a5744ff7c4f9d7548ca10a5da9a5f389
leonardomso <leonardomso11@gmail.com>
2018-10-03 15:18:15 -0300

    Using graphql-tools

--- server/schema/Schema.js
+++ server/schema/Schema.js
@@ -1,108 +1,50 @@
-const graphql = require("graphql");
-const axios = require("axios");
-const express = require("express");
-const mongoose = require("mongoose");
+const { makeExecutableSchema } = require("graphql-tools");
 
 const User = require("../models/User");
 
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLNonNull
-} = graphql;
+const typeDefs = `
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
 
-// Create User schema.
-const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: {
-    id: { type: GraphQLInt },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString }
-  }
-});
+    type Query {    
+        user(id: ID): User
+        users: [User]
+    }
 
-// Create rootQuery.
-const query = new GraphQLObjectType({
-  name: "query",
-  fields: {
-    user: {
-      type: UserType,
-      args: {
-        id: { type: GraphQLInt },
-        name: { type: GraphQLString },
-        email: { type: GraphQLString }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .get(`http://localhost:4000/users/${args.id}`)
-          .then(response => response.data)
-          .catch(err => response.data.err);
+    type Mutation {
+        addUser (id: ID!, name: String!, email: String!): User
+        editUser (id: ID!, name: String!, email: String!): User
+        deleteUser (id: ID!): User        
+    }
+`;
+
+const resolvers = {
+  Query: {
+    users: user => {
+      return;
+    },
+    user: async (parent, { userId }) => {
+      try {
+        const user = await User.findOne({ id: userId }).lean();
+        return user;
+      } catch (err) {
+        throw new Error(err);
       }
     }
+  },
+  Mutation: {
+    addUser(_, { userId, userName, userEmail }) {},
+    editUser(_, { userId, userName, userEmail }) {},
+    deleteUser(_, { userId }) {}
   }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
 
-const mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    addUser: {
-      type: UserType,
-      args: {
-        id: {
-          type: new GraphQLNonNull(GraphQLInt)
-        },
-        name: {
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        email: {
-          type: new GraphQLNonNull(GraphQLString)
-        }
-      },
-      resolve(parentValue, { id, name, email }) {
-        return axios
-          .post(`http://localhost:4000/users`, { id, name, email })
-          .then(response => response.data)
-          .catch(err => response.data.err);
-      }
-    },
-    deleteUser: {
-      type: UserType,
-      args: {
-        id: { type: GraphQLNonNull(GraphQLString) }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .delete(`http://localhost:4000/users/${args.id}`)
-          .then(response => response.data)
-          .catch(err => response.data.err);
-      }
-    },
-    editUser: {
-      type: UserType,
-      args: {
-        name: {
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        email: {
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        password: {
-          type: new GraphQLNonNull(GraphQLString)
-        }
-      },
-      resolve(parentValue, args) {
-        return axios
-          .patch(`http://localhost:4000/users/${args.name}`, args)
-          .then(response => response.data)
-          .catch(err => response.data.err);
-      }
-    }
-  }
-});
-
-module.exports = new GraphQLSchema({
-  query,
-  mutation
-});
+module.exports = schema;
